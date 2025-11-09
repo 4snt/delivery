a7e6553bd872c956b289441d479b47f9759839a2
MuriloSantiago <murilo.santiago@gafit.com.br>
2025-11-09 13:58:46 -0300

    fix(migrations): convert all SQLite migrations to PostgreSQL syntax

--- prisma/migrations/20251109130904_init/migration.sql
+++ prisma/migrations/20251109130904_init/migration.sql
@@ -1,50 +1,53 @@
 -- CreateTable
 CREATE TABLE "Adicional" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "nome" TEXT NOT NULL
+    "id" BIGSERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+
+    CONSTRAINT "Adicional_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Sabor" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" BIGSERIAL NOT NULL,
     "nome" TEXT NOT NULL,
-    "imagem" TEXT NOT NULL
+    "imagem" TEXT NOT NULL,
+
+    CONSTRAINT "Sabor_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Cliente" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" BIGSERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "nome" TEXT NOT NULL,
-    "senha" TEXT NOT NULL
+    "senha" TEXT NOT NULL,
+
+    CONSTRAINT "Cliente_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Pedido" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "clienteId" INTEGER NOT NULL,
+    "id" BIGSERIAL NOT NULL,
+    "clienteId" BIGINT NOT NULL,
     "tamanho" TEXT NOT NULL,
-    "valorTotal" REAL NOT NULL,
+    "valorTotal" DOUBLE PRECISION NOT NULL,
     "formaPagamento" TEXT NOT NULL,
     "enderecoEntrega" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "Pedido_clienteId_fkey" FOREIGN KEY ("clienteId") REFERENCES "Cliente" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Pedido_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "_PedidoAdicionais" (
-    "A" INTEGER NOT NULL,
-    "B" INTEGER NOT NULL,
-    CONSTRAINT "_PedidoAdicionais_A_fkey" FOREIGN KEY ("A") REFERENCES "Adicional" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "_PedidoAdicionais_B_fkey" FOREIGN KEY ("B") REFERENCES "Pedido" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    "A" BIGINT NOT NULL,
+    "B" BIGINT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "_PedidoToSabor" (
-    "A" INTEGER NOT NULL,
-    "B" INTEGER NOT NULL,
-    CONSTRAINT "_PedidoToSabor_A_fkey" FOREIGN KEY ("A") REFERENCES "Pedido" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "_PedidoToSabor_B_fkey" FOREIGN KEY ("B") REFERENCES "Sabor" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    "A" BIGINT NOT NULL,
+    "B" BIGINT NOT NULL
 );
 
 -- CreateIndex
@@ -61,3 +64,18 @@ CREATE UNIQUE INDEX "_PedidoToSabor_AB_unique" ON "_PedidoToSabor"("A", "B");
 
 -- CreateIndex
 CREATE INDEX "_PedidoToSabor_B_index" ON "_PedidoToSabor"("B");
+
+-- AddForeignKey
+ALTER TABLE "Pedido" ADD CONSTRAINT "Pedido_clienteId_fkey" FOREIGN KEY ("clienteId") REFERENCES "Cliente"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PedidoAdicionais" ADD CONSTRAINT "_PedidoAdicionais_A_fkey" FOREIGN KEY ("A") REFERENCES "Adicional"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PedidoAdicionais" ADD CONSTRAINT "_PedidoAdicionais_B_fkey" FOREIGN KEY ("B") REFERENCES "Pedido"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PedidoToSabor" ADD CONSTRAINT "_PedidoToSabor_A_fkey" FOREIGN KEY ("A") REFERENCES "Pedido"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PedidoToSabor" ADD CONSTRAINT "_PedidoToSabor_B_fkey" FOREIGN KEY ("B") REFERENCES "Sabor"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20251109132110_fix_many_to_many/migration.sql
+++ prisma/migrations/20251109132110_fix_many_to_many/migration.sql
@@ -5,32 +5,48 @@
   - You are about to drop the `_PedidoToSabor` table. If the table is not empty, all the data it contains will be lost.
 
 */
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "_PedidoAdicionais";
-PRAGMA foreign_keys=on;
+-- DropForeignKey
+ALTER TABLE "_PedidoAdicionais" DROP CONSTRAINT "_PedidoAdicionais_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_PedidoAdicionais" DROP CONSTRAINT "_PedidoAdicionais_B_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_PedidoToSabor" DROP CONSTRAINT "_PedidoToSabor_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_PedidoToSabor" DROP CONSTRAINT "_PedidoToSabor_B_fkey";
 
 -- DropTable
-PRAGMA foreign_keys=off;
+DROP TABLE "_PedidoAdicionais";
+
+-- DropTable
 DROP TABLE "_PedidoToSabor";
-PRAGMA foreign_keys=on;
 
 -- CreateTable
 CREATE TABLE "PedidoSabor" (
-    "pedidoId" INTEGER NOT NULL,
-    "saborId" INTEGER NOT NULL,
+    "pedidoId" BIGINT NOT NULL,
+    "saborId" BIGINT NOT NULL,
 
-    PRIMARY KEY ("pedidoId", "saborId"),
-    CONSTRAINT "PedidoSabor_pedidoId_fkey" FOREIGN KEY ("pedidoId") REFERENCES "Pedido" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "PedidoSabor_saborId_fkey" FOREIGN KEY ("saborId") REFERENCES "Sabor" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "PedidoSabor_pkey" PRIMARY KEY ("pedidoId","saborId")
 );
 
 -- CreateTable
 CREATE TABLE "PedidoAdicional" (
-    "pedidoId" INTEGER NOT NULL,
-    "adicionalId" INTEGER NOT NULL,
+    "pedidoId" BIGINT NOT NULL,
+    "adicionalId" BIGINT NOT NULL,
 
-    PRIMARY KEY ("pedidoId", "adicionalId"),
-    CONSTRAINT "PedidoAdicional_pedidoId_fkey" FOREIGN KEY ("pedidoId") REFERENCES "Pedido" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "PedidoAdicional_adicionalId_fkey" FOREIGN KEY ("adicionalId") REFERENCES "Adicional" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "PedidoAdicional_pkey" PRIMARY KEY ("pedidoId","adicionalId")
 );
+
+-- AddForeignKey
+ALTER TABLE "PedidoSabor" ADD CONSTRAINT "PedidoSabor_pedidoId_fkey" FOREIGN KEY ("pedidoId") REFERENCES "Pedido"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PedidoSabor" ADD CONSTRAINT "PedidoSabor_saborId_fkey" FOREIGN KEY ("saborId") REFERENCES "Sabor"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PedidoAdicional" ADD CONSTRAINT "PedidoAdicional_pedidoId_fkey" FOREIGN KEY ("pedidoId") REFERENCES "Pedido"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PedidoAdicional" ADD CONSTRAINT "PedidoAdicional_adicionalId_fkey" FOREIGN KEY ("adicionalId") REFERENCES "Adicional"("id") ON DELETE CASCADE ON UPDATE CASCADE;
